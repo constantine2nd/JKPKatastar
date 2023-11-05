9513eea812d263640742d252a2a3c117ca40aaa7
Marko Milić <marko@tesobe.com>
2023-11-05 09:22:28 +0100

    Add translations and remove warning at landing page

--- client/src/screens/LandingScreen.tsx
+++ client/src/screens/LandingScreen.tsx
@@ -2,26 +2,8 @@ import React, { useState, useEffect, useRef, useMemo } from "react";
 import { useNavigate, createSearchParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useTranslation } from "react-i18next";
-// MUI Table
-import { styled } from "@mui/material/styles";
-import { darken } from "@mui/material";
-import TableMUI from "@mui/material/Table";
-import TableBodyMUI from "@mui/material/TableBody";
-import TableCellMUI, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainerMUI from "@mui/material/TableContainer";
-import TableHeadMUI from "@mui/material/TableHead";
-import TableRowMUI from "@mui/material/TableRow";
-import PaperMUI from "@mui/material/Paper";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-
-import { MaterialReactTable, type MRT_ColumnDef } from "material-react-table";
-//Import Material React Table Translations
-import { MRT_Localization_SR_CYRL_RS } from "material-react-table/locales/sr-Cyrl-RS";
-//Import Material React Table Translations
-import { MRT_Localization_SR_LATN_RS } from "material-react-table/locales/sr-Latn-RS";
-//Import Material React Table Translations
-import { MRT_Localization_HU } from "material-react-table/locales/hu";
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -42,12 +24,14 @@ const LandingScreen: React.FC = () => {
   const [cemeteryId, setCemeteryId] = React.useState("");
   let navigate = useNavigate();
 
+  const { t, i18n } = useTranslation();
+
   const handleChange = (event: SelectChangeEvent) => {
     const selectedCemetery = cemeteries?.find(
       (cem) => cem._id === event.target.value
     );
     navigate("/", { state: { cemetery: selectedCemetery } });
-    // setCemeteryId(event.target.value as string);
+    setCemeteryId(event.target.value as string);
   };
 
   const usersStatus = useSelector(getAllCemeteriesStatus);
@@ -74,19 +58,19 @@ const LandingScreen: React.FC = () => {
 
   return (
     <>
-      <h1>Landing page screen</h1>
+      <h1>{t("Landing page screen")}</h1>
       <Box sx={{ minWidth: 120 }}>
         <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Groblje</InputLabel>
+          <InputLabel id="cemetery-select-label">{t("Cemetery")}</InputLabel>
           <Select
-            labelId="demo-simple-select-label"
+            labelId="cemetery-select-label"
             id="demo-simple-select"
             value={cemeteryId}
-            label="Age"
+            label="Cemetery"
             onChange={handleChange}
           >
             {cemeteries?.map((cemetery) => (
-              <MenuItem value={cemetery._id}>{cemetery.name}</MenuItem>
+              <MenuItem key={cemetery._id} value={cemetery._id}>{cemetery.name}</MenuItem>
             ))}
           </Select>
         </FormControl>
