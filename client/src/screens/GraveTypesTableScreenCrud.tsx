85b8d7255fe5dc7892a1a3ca33ebabd9cef646ef
Marko Milić <marko@tesobe.com>
2023-12-02 12:37:45 +0100

    Tweak grave types crud

--- client/src/screens/GraveTypesTableScreenCrud.tsx
+++ client/src/screens/GraveTypesTableScreenCrud.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
   MRT_EditActionButtons,
   MaterialReactTable,
@@ -7,7 +7,7 @@ import {
   type MRT_Row,
   type MRT_TableOptions,
   useMaterialReactTable,
-} from 'material-react-table';
+} from "material-react-table";
 import {
   Box,
   Button,
@@ -16,18 +16,17 @@ import {
   DialogTitle,
   IconButton,
   Tooltip,
-} from '@mui/material';
-import {
-  useMutation,
-  useQuery,
-  useQueryClient,
-} from '@tanstack/react-query';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+} from "@mui/material";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { useTranslation } from "react-i18next";
-import axios from 'axios';
+import axios from "axios";
 import { getLanguage } from "../utils/languageSelector";
-import { GraveType } from '../interfaces/GraveTypeInterfaces';
+import { GraveType } from "../interfaces/GraveTypeInterfaces";
+import { t } from "i18next";
+
+const graveTypeQueryKey = "grave-types-all";
 
 const GraveTypesTableScreenCrud = () => {
   const [validationErrors, setValidationErrors] = useState<
@@ -37,89 +36,82 @@ const GraveTypesTableScreenCrud = () => {
   const { t, i18n } = useTranslation();
 
   const columns: MRT_ColumnDef<GraveType>[] = [
-      {
-        accessorKey: '_id',
-        header: 'Id',
-        enableEditing: false,
-        size: 80,
+    {
+      accessorKey: "_id",
+      header: "Id",
+      enableEditing: false,
+      size: 80,
+    },
+    {
+      accessorKey: "name",
+      header: t("name"),
+      muiEditTextFieldProps: {
+        type: "text",
+        required: true,
+        error: !!validationErrors?.name,
+        helperText: validationErrors?.name,
+        //remove any previous validation errors when user focuses on the input
+        onFocus: () =>
+          setValidationErrors({
+            ...validationErrors,
+            name: undefined,
+          }),
+        //optionally add validation checking for onBlur or onChange
       },
-      {
-        accessorKey: 'name',
-        header: t('name'),
-        muiEditTextFieldProps: {
-          type: 'text',
-          required: true,
-          error: !!validationErrors?.name,
-          helperText: validationErrors?.name,
-          //remove any previous validation errors when user focuses on the input
-          onFocus: () =>
-            setValidationErrors({
-              ...validationErrors,
-              name: undefined,
-            }),
-          //optionally add validation checking for onBlur or onChange
-        },
-      },
-      {
-        accessorKey: 'capacity',
-        header: t('capacity'),
-      },
-      {
-        accessorKey: 'description',
-        header: t('description'),
-      },
-    ];
+    },
+    {
+      accessorKey: "capacity",
+      header: t("capacity"),
+    },
+    {
+      accessorKey: "description",
+      header: t("description"),
+    },
+  ];
 
-  //call CREATE hook
-  const { mutateAsync: createRow, isPending: isCreatingRow } =
-    useGraveType();
-  //call READ hook
+  // call CREATE hook
+  const { mutateAsync: createRow, isPending: isCreatingRow } = useCreateRow();
+  // call READ hook
   const {
     data: fetchedData = [],
     isError: isLoadingDataError,
     isFetching: isFetchingData,
     isLoading: isLoadingData,
-  } = useGetGraveTypes();
-  //call UPDATE hook
-  const { mutateAsync: updateRow, isPending: isUpdatingRow } =
-    useUpdateGraveType();
-  //call DELETE hook
-  const { mutateAsync: deleteRow, isPending: isDeletingRow } =
-    useDeleteGraveType();
+  } = useGetRows();
+  // call UPDATE hook
+  const { mutateAsync: updateRow, isPending: isUpdatingRow } = useUpdateRow();
+  // call DELETE hook
+  const { mutateAsync: deleteRow, isPending: isDeletingRow } = useDeleteRow();
 
-  //CREATE action
-  const handleCreateGraveType: MRT_TableOptions<GraveType>['onCreatingRowSave'] = async ({
-    values,
-    table,
-  }) => {
-    const newValidationErrors = validateGraveType(values);
-    if (Object.values(newValidationErrors).some((error) => error)) {
-      setValidationErrors(newValidationErrors);
-      return;
-    }
-    setValidationErrors({});
-    await createRow(values);
-    table.setCreatingRow(null); //exit creating mode
-  };
+  // CREATE action
+  const handleCreateGraveType: MRT_TableOptions<GraveType>["onCreatingRowSave"] =
+    async ({ values, table }) => {
+      const newValidationErrors = validateGraveType(values);
+      if (Object.values(newValidationErrors).some((error) => error)) {
+        setValidationErrors(newValidationErrors);
+        return;
+      }
+      setValidationErrors({});
+      await createRow(values);
+      table.setCreatingRow(null); //exit creating mode
+    };
 
-  //UPDATE action
-  const handleSaveRow: MRT_TableOptions<GraveType>['onEditingRowSave'] = async ({
-    values,
-    table,
-  }) => {
-    const newValidationErrors = validateGraveType(values);
-    if (Object.values(newValidationErrors).some((error) => error)) {
-      setValidationErrors(newValidationErrors);
-      return;
-    }
-    setValidationErrors({});
-    await updateRow(values);
-    table.setEditingRow(null); //exit editing mode
-  };
+  // UPDATE action
+  const handleSaveRow: MRT_TableOptions<GraveType>["onEditingRowSave"] =
+    async ({ values, table }) => {
+      const newValidationErrors = validateGraveType(values);
+      if (Object.values(newValidationErrors).some((error) => error)) {
+        setValidationErrors(newValidationErrors);
+        return;
+      }
+      setValidationErrors({});
+      await updateRow(values);
+      table.setEditingRow(null); //exit editing mode
+    };
 
-  //DELETE action
+  // DELETE action
   const openDeleteConfirmModal = (row: MRT_Row<GraveType>) => {
-    if (window.confirm('Are you sure you want to delete this user?')) {
+    if (window.confirm("Are you sure you want to delete this user?")) {
       deleteRow(row.original._id);
     }
   };
@@ -128,19 +120,19 @@ const GraveTypesTableScreenCrud = () => {
     columns,
     data: fetchedData,
     localization: getLanguage(i18n),
-    createDisplayMode: 'modal', //default ('row', and 'custom' are also available)
-    editDisplayMode: 'modal', //default ('row', 'cell', 'table', and 'custom' are also available)
+    createDisplayMode: "modal", //default ('row', and 'custom' are also available)
+    editDisplayMode: "modal", //default ('row', 'cell', 'table', and 'custom' are also available)
     enableEditing: true,
     getRowId: (row) => row._id,
     muiToolbarAlertBannerProps: isLoadingDataError
       ? {
-          color: 'error',
-          children: 'Error loading data',
+          color: "error",
+          children: "Error loading data",
         }
       : undefined,
     muiTableContainerProps: {
       sx: {
-        minHeight: '500px',
+        minHeight: "500px",
       },
     },
     onCreatingRowCancel: () => setValidationErrors({}),
@@ -152,11 +144,11 @@ const GraveTypesTableScreenCrud = () => {
       <>
         <DialogTitle variant="h3">{t("Create New Grave Type")}</DialogTitle>
         <DialogContent
-          sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}
+          sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}
         >
           {internalEditComponents} {/* or render custom edit components here */}
         </DialogContent>
-        <DialogActions >
+        <DialogActions>
           <MRT_EditActionButtons variant="text" table={table} row={row} />
         </DialogActions>
       </>
@@ -166,7 +158,7 @@ const GraveTypesTableScreenCrud = () => {
       <>
         <DialogTitle variant="h3">{t("Edit Grave Type")}</DialogTitle>
         <DialogContent
-          sx={{ display: 'flex', flexDirection: 'column', gap: '1.5rem' }}
+          sx={{ display: "flex", flexDirection: "column", gap: "1.5rem" }}
         >
           {internalEditComponents} {/* or render custom edit components here */}
         </DialogContent>
@@ -176,7 +168,7 @@ const GraveTypesTableScreenCrud = () => {
       </>
     ),
     renderRowActions: ({ row, table }) => (
-      <Box sx={{ display: 'flex', gap: '1rem' }}>
+      <Box sx={{ display: "flex", gap: "1rem" }}>
         <Tooltip title={t("Edit")}>
           <IconButton onClick={() => table.setEditingRow(row)}>
             <EditIcon />
@@ -216,54 +208,11 @@ const GraveTypesTableScreenCrud = () => {
   return <MaterialReactTable table={table} />;
 };
 
-//CREATE hook (post new user to api)
-function useGraveType() {
+// CREATE hook (post a new row to api)
+function useCreateRow() {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: async (graveType: GraveType) => {
-      //send api update request here
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-      const dataToSend = { 
-        name: graveType.name,
-        capacity: graveType.capacity,
-        description: graveType.description,
-      }
-      const response = await axios.post(`/api/grave-types/addgravetype`, dataToSend, config);
-      return response.data;
-    },
-    //client side optimistic update
-    onSuccess: (newRowInfo: GraveType) => {
-      queryClient.setQueryData(
-        ['grave-types-all'],
-        (prevRows: any) => [...prevRows, {...newRowInfo,},] as GraveType[],
-      );
-    },
-    // onSettled: () => queryClient.invalidateQueries({ queryKey: ['users-all'] }), //refetch users after mutation, disabled for demo
-  });
-}
-
-//READ hook (get grave types from api)
-function useGetGraveTypes() {
-  return useQuery<GraveType[]>({
-    queryKey: ['grave-types-all'],
-    queryFn: async () => {
-      // send api request here 
-      const response = await axios.get(`/api/grave-types/all`);
-      return response.data;
-    },
-    refetchOnWindowFocus: true,
-  });
-}
-
-//UPDATE hook (put user in api)
-function useUpdateGraveType() {
-  const queryClient = useQueryClient();
-  return useMutation({
-    mutationFn: async (graveType: GraveType) => {
+    mutationFn: async (row: GraveType) => {
       //send api update request here
       const config = {
         headers: {
@@ -271,30 +220,79 @@ function useUpdateGraveType() {
         },
       };
       const dataToSend = {
-        id: graveType._id,
-        name: graveType.name,
-        description: graveType.description,
-        capacity: graveType.capacity,
-      }
-      const response = await axios.put(`/api/grave-types/updategravetype`, dataToSend, config);
+        name: row.name,
+        capacity: row.capacity,
+        description: row.description,
+      };
+      const response = await axios.post(
+        `/api/grave-types/addgravetype`,
+        dataToSend,
+        config
+      );
+      return response.data;
+    },
+    //client side optimistic update
+    onSuccess: (newRowInfo: GraveType) => {
+      queryClient.setQueryData(
+        [graveTypeQueryKey],
+        (prevRows: any) => [...prevRows, { ...newRowInfo }] as GraveType[]
+      );
+    },
+    // onSettled: () => queryClient.invalidateQueries({ queryKey: [graveTypeQueryKey] }), //refetch users after mutation, disabled for demo
+  });
+}
+
+// READ hook (get rows from api)
+function useGetRows() {
+  return useQuery<GraveType[]>({
+    queryKey: [graveTypeQueryKey],
+    queryFn: async () => {
+      // send api request here
+      const response = await axios.get(`/api/grave-types/all`);
+      return response.data;
+    },
+    refetchOnWindowFocus: true,
+  });
+}
+
+// UPDATE hook (put a row in api)
+function useUpdateRow() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (row: GraveType) => {
+      //send api update request here
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const dataToSend = {
+        id: row._id,
+        name: row.name,
+        description: row.description,
+        capacity: row.capacity,
+      };
+      const response = await axios.put(
+        `/api/grave-types/updategravetype`,
+        dataToSend,
+        config
+      );
       return response.data;
     },
     //client side optimistic update
     onMutate: (newGraveTypeInfo: GraveType) => {
-      queryClient.setQueryData(
-        ['grave-types-all'],
-        (prevRows: any) =>
-          prevRows?.map((row: GraveType) =>
-            row._id === newGraveTypeInfo._id ? newGraveTypeInfo : row,
-          ),
+      queryClient.setQueryData([graveTypeQueryKey], (prevRows: any) =>
+        prevRows?.map((row: GraveType) =>
+          row._id === newGraveTypeInfo._id ? newGraveTypeInfo : row
+        )
       );
     },
-    // onSettled: () => queryClient.refetchQueries({ queryKey: ['users-all'] }), //refetch users after mutation, disabled for demo
+    // onSettled: () => queryClient.refetchQueries({ queryKey: [graveTypeQueryKey] }), //refetch users after mutation, disabled for demo
   });
 }
 
-//DELETE hook (delete grave type in api)
-function useDeleteGraveType() {
+// DELETE hook (delete a row in api)
+function useDeleteRow() {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: async (id: string) => {
@@ -302,12 +300,10 @@ function useDeleteGraveType() {
       const response = await axios.delete(`/api/grave-types/${id}`);
       return response.data;
     },
-    //client side optimistic update
+    // client side optimistic update
     onMutate: (id: string) => {
-      queryClient.setQueryData(
-        ['grave-types-all'],
-        (prevRows: any) =>
-          prevRows?.filter((row: GraveType) => row._id !== id),
+      queryClient.setQueryData([graveTypeQueryKey], (prevRows: any) =>
+        prevRows?.filter((row: GraveType) => row._id !== id)
       );
     },
     // onSettled: () => queryClient.invalidateQueries({ queryKey: ['users-all'] }), //refetch users after mutation, disabled for demo
@@ -322,8 +318,8 @@ export default GraveTypesTableScreenCrudWithProviders;
 
 const validateRequired = (value: string) => !!value.length;
 
-function validateGraveType(graveType: GraveType) {
+function validateGraveType(row: GraveType) {
   return {
-    name: !validateRequired(graveType.name) ? 'Name is Required' : '',
+    name: !validateRequired(row.name) ? t("Name is Required") : "",
   };
 }
