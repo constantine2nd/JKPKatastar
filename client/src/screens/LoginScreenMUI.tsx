1fed3f61d8cbb8cf86fdf1def916554c880ae551
Marko Milić <marko@tesobe.com>
2024-01-30 20:31:32 +0100

    Add sign up link at login page

--- client/src/screens/LoginScreenMUI.tsx
+++ client/src/screens/LoginScreenMUI.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
@@ -11,7 +10,6 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
@@ -126,7 +124,7 @@ export default function SignIn() {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/add-user" variant="body2">
                 {t("Don't have an account? Sign Up")}
               </Link>
             </Grid>
