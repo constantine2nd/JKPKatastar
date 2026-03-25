a9464c81667d3c0239918fa5c5c697aef626cf28
Marko Milić <marko@tesobe.com>
2026-03-25 07:47:10 +0100

    Replace cemetery dropdown with responsive card grid on landing screen

--- client/src/screens/LandingScreen.tsx
+++ client/src/screens/LandingScreen.tsx
@@ -1,14 +1,15 @@
-import React, { useState, useEffect, useRef, useMemo } from "react";
-import { useNavigate, createSearchParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useTranslation } from "react-i18next";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 import {
   fetchCemeteries,
@@ -23,28 +24,24 @@ import { getSelectedCemetery } from "../utils/cemeterySelector";
 const LandingScreen: React.FC = () => {
   const cemeteries: Cemetery[] | null = useSelector(selectAllCemeteries);
   const [cemeteryId, setCemeteryId] = React.useState(getSelectedCemetery());
-  let navigate = useNavigate();
-  console.log(cemeteryId);
-  const { t, i18n } = useTranslation();
-
-  const handleChange = (event: SelectChangeEvent) => {
-    const selectedCemetery = cemeteries?.find(
-      (cem) => cem._id === event.target.value,
-    );
-    navigate("/", { state: { cemetery: selectedCemetery } });
-    setCemeteryId(event.target.value);
-    localStorage.setItem("selected-cemetery", JSON.stringify(selectedCemetery));
-  };
+  const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const usersStatus = useSelector(getAllCemeteriesStatus);
   const error = useSelector(getAllCemeteriesError);
   const dispatch = useDispatch<any>();
+
   useEffect(() => {
     if (usersStatus === "idle") {
-      console.log("UPAO");
       dispatch(fetchCemeteries());
     }
   }, [usersStatus, dispatch]);
+
+  const handleSelect = (cemetery: Cemetery) => {
+    setCemeteryId(cemetery._id);
+    localStorage.setItem("selected-cemetery", JSON.stringify(cemetery));
+    navigate("/", { state: { cemetery } });
+  };
 
   if (usersStatus === "loading") {
     return <Loader />;
@@ -59,33 +56,36 @@ const LandingScreen: React.FC = () => {
   }
 
   return (
-    <>
-      <h1>{t("client.landing-page-screen")}</h1>
-      <Box sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
-          <InputLabel id="cemetery-select-label">
-            {t("cemetery.title")}
-          </InputLabel>
-          <Select
-            labelId="cemetery-select-label"
-            id="demo-simple-select"
-            value={cemeteryId}
-            label={t("cemetery.title")}
-            onChange={handleChange}
-          >
-            {cemeteries?.map((cemetery) => (
-              <MenuItem key={cemetery._id} value={cemetery._id}>
-                {cemetery.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      </Box>
-    </>
+    <Box sx={{ p: 2 }}>
+      <Typography variant="h5" gutterBottom>
+        {t("client.landing-page-screen")}
+      </Typography>
+      <Grid container spacing={2}>
+        {cemeteries?.map((cemetery) => {
+          const selected = cemetery._id === cemeteryId;
+          return (
+            <Grid item xs={12} sm={6} md={4} key={cemetery._id}>
+              <Card
+                variant="outlined"
+                sx={{
+                  borderColor: selected ? "primary.main" : "divider",
+                  borderWidth: selected ? 2 : 1,
+                }}
+              >
+                <CardActionArea onClick={() => handleSelect(cemetery)}>
+                  <CardContent>
+                    <Typography variant="h6" component="div">
+                      {cemetery.name}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
   );
 };
 
 export default LandingScreen;
-function createTheme(arg0: { palette: { mode: string } }) {
-  throw new Error("Function not implemented.");
-}
